61f751e28083c145b373263679091cbe997e288e
Robert Fryman <robert.fryman@gmail.com>
2017-03-26 13:37:42 -0500

    Add tier css

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
   def index
     @beers = Beer.all.includes(:tier)
-    @tiers = Tier.all
+    @tiers = Tier.all.order(:id)
     @guest_beers = GuestBeer.all
   end
 end

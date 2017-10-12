daac35ea5a0ce1c5219a50303309e97a9425109d
Brandon Valentine <brandon@brandonvalentine.com>
2017-10-12 12:46:01 -0500

    Allow StaticController#index to return a JSON array

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,5 +3,10 @@ class StaticController < ApplicationController
     @beers = Beer.where(active: true).includes(:tier).order(:position)
     @tiers = Tier.where(hide: false).order(:id)
     @guest_beers = GuestBeer.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: { beers: @beers, tiers: @tiers, guest_beers: @guest_beers } }
+    end
   end
 end

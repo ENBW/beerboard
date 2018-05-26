fddd8b6dc8b485188cea3d4d097586aa396b88be
Brandon Valentine <brandon@brandonvalentine.com>
2018-05-26 10:53:34 -0500

    Remove rails_admin and add activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :admins, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root to: 'static#index'
 end

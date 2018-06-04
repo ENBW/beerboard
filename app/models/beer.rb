e4d7291431753b1191d5f67aa6d48c2ddbdd5157
Brandon Valentine <brandon@brandonvalentine.com>
2018-06-04 00:54:59 -0500

    Add image_url to JSON endpoint
    Fixes #11

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,4 +4,12 @@ class Beer < ApplicationRecord
   has_one_attached :image
 
   acts_as_list
+
+  def image_url
+    Rails.application.routes.url_helpers.url_for(image) if image.attached?
+  end
+
+  def as_json(options= nil)
+    super(methods: [:image_url])
+  end
 end

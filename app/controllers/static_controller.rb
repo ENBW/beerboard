7830a37c5dfe5da9efc8ca60dced0bff0f87e415
Stephen Yeargin <stephen.yeargin@gmail.com>
2018-07-13 17:36:49 -0500

    Drop Guest Beer model, updates Gems, adjust CSS
    - Drops the Guest Beer model in favor of having the three other items static in the layout (they don't change as much)
    - Removes `bumlma-rails` from the `Gemfile` and updates other relevant gems to their latest releases
    - Minor adjustments to the CSS using [Chrome's responsive previewer](https://developers.google.com/web/tools/chrome-devtools/device-mode/) to make sure things will look great on the TV.
    - Whitespace cleanup in the SCSS file

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,11 +2,10 @@ class StaticController < ApplicationController
   def index
     @beers = Beer.where(active: true).includes(:tier).order(:position)
     @tiers = Tier.where(hide: false).order(:id)
-    @guest_beers = GuestBeer.all
 
     respond_to do |format|
       format.html
-      format.json { render json: { beers: @beers, tiers: @tiers, guest_beers: @guest_beers } }
+      format.json { render json: { beers: @beers, tiers: @tiers } }
     end
   end
 end

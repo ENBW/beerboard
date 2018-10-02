b34e0572047d628b508b702834c99f8d6afee974
Stephen Yeargin <stephen.yeargin@gmail.com>
2018-10-02 00:24:18 -0500

    [WIP] Split screen view
    This PR splits the beer board into two routes, one for each of the main tiers.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,11 +1,23 @@
 class StaticController < ApplicationController
-  def index
-    @beers = Beer.where(active: true).includes(:tier).order(:position)
-    @tiers = Tier.where(hide: false).order(:id)
+  before_action :load_data
 
+  def index
     respond_to do |format|
       format.html
       format.json { render json: { beers: @beers, tiers: @tiers } }
     end
   end
+
+  def vertical
+    tier_id = params[:tier_id]
+    render "tier_#{tier_id}.html.erb"
+  end
+
+  private
+  
+  def load_data
+    @beers = Beer.where(active: true).includes(:tier).order(:position)
+    @tiers = Tier.where(hide: false).order(:id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :admins, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'static#index'
+  get '/tier_:tier_id', to: 'static#vertical'
 end
